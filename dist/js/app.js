cd290aa41aab332c1d59f3d96481297d58c1a30a
Raúl Lozano García <59658847+Raul-Lozano-Garcia@users.noreply.github.com>
2021-06-04 12:42:08 +0200

    Add files via upload

--- dist/js/app.js
+++ dist/js/app.js
@@ -1,7 +1,9 @@
 const navToggle = document.querySelector('#nav-toggle');
 const navLinks = document.querySelectorAll('#nav ul li a');
 const navContainer = document.querySelector('#header .container');
-//ver si puedo quitar la clase nav links
+const darkToggle = document.querySelector("#dark");
+const colorTags= document.querySelectorAll(".color");
+
 navToggle.addEventListener('click', () => {
     navContainer.classList.toggle('nav-open');
 });
@@ -11,3 +13,9 @@ navLinks.forEach(link => {
         navContainer.classList.remove('nav-open');
     })
 })
+
+darkToggle.addEventListener('click', () => {
+    colorTags.forEach(color => {
+        color.classList.toggle('dark');
+    })
+});
